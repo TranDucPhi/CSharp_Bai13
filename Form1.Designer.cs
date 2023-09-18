6d2177364841a450a0a6e3f26d88ff1a40f914d1
ADMIN <ADMIN@LAB1-MAY33>
2023-09-18 15:02:34 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CSharp_Bai13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.btnHo = new System.Windows.Forms.Button();
+            this.btnTen = new System.Windows.Forms.Button();
+            this.btnHoTen = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Controls.Add(this.lblHoTen);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(976, 95);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ Lót";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên";
+            // 
+            // txtHo
+            // 
+            this.txtHo.Location = new System.Drawing.Point(95, 110);
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(432, 20);
+            this.txtHo.TabIndex = 2;
+            this.txtHo.TextChanged += new System.EventHandler(this.txtHo_TextChanged);
+            // 
+            // txtTen
+            // 
+            this.txtTen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtTen.Location = new System.Drawing.Point(95, 152);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(432, 20);
+            this.txtTen.TabIndex = 4;
+            // 
+            // btnHo
+            // 
+            this.btnHo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHo.Location = new System.Drawing.Point(51, 206);
+            this.btnHo.Name = "btnHo";
+            this.btnHo.Size = new System.Drawing.Size(90, 32);
+            this.btnHo.TabIndex = 5;
+            this.btnHo.Text = "Họ Lót";
+            this.btnHo.UseVisualStyleBackColor = true;
+            this.btnHo.Click += new System.EventHandler(this.btnHo_Click);
+            // 
+            // btnTen
+            // 
+            this.btnTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTen.Location = new System.Drawing.Point(228, 206);
+            this.btnTen.Name = "btnTen";
+            this.btnTen.Size = new System.Drawing.Size(90, 32);
+            this.btnTen.TabIndex = 6;
+            this.btnTen.Text = "Tên";
+            this.btnTen.UseVisualStyleBackColor = true;
+            this.btnTen.Click += new System.EventHandler(this.btnTen_Click);
+            // 
+            // btnHoTen
+            // 
+            this.btnHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoTen.Location = new System.Drawing.Point(411, 206);
+            this.btnHoTen.Name = "btnHoTen";
+            this.btnHoTen.Size = new System.Drawing.Size(90, 32);
+            this.btnHoTen.TabIndex = 7;
+            this.btnHoTen.Text = "Họ Và Tên";
+            this.btnHoTen.UseVisualStyleBackColor = true;
+            this.btnHoTen.Click += new System.EventHandler(this.btnHoTen_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(199, 275);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(129, 36);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Thoát Chương Trình";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblHoTen.Location = new System.Drawing.Point(86, 30);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(0, 39);
+            this.lblHoTen.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnHoTen;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(539, 323);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHoTen);
+            this.Controls.Add(this.btnTen);
+            this.Controls.Add(this.btnHo);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Họ Tên";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Button btnHo;
+        private System.Windows.Forms.Button btnTen;
+        private System.Windows.Forms.Button btnHoTen;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblHoTen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Bai13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r;
+            r = MessageBox.Show("Bạn Muốn Thoát?", "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1);
+            if (r == DialogResult.No)
+                e.Cancel = true;
+        }
+        private void txtHo_TextChanged(object sender, EventArgs e){ }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnHo_Click(object sender, EventArgs e)
+        {
+            Text = txtHo.Text;
+            //string[] arr= Text.Split(' ');
+            //txtHo.Text = arr[0];
+            //txtHo.Text = arr[1];
+            lblHoTen.Text = txtHo.Text;
+        }
+        private void btnTen_Click(object sender, EventArgs e)
+        {
+            lblHoTen.Text += " " + txtTen.Text;
+        }
+        private void btnHoTen_Click(object sender, EventArgs e)
+        {
+            Text = txtHo.Text;
+            string[] arr = Text.Split(' ');
+            lblHoTen.Text = txtHo.Text;
+            lblHoTen.Text = txtHo.Text +" "+ txtTen.Text;
+        }
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            lblHoTen.Text = "";
+            txtHo.Clear();
+            txtTen.Clear();
+            txtHo.Focus();
+        }
+    }
+}
